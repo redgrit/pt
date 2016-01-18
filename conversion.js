6a3067832bfc534f105fc961d2f21b9e7c145051
GeoffreyLund <g.lund@redgrit.net>
2016-01-18 00:10:59 +0000

    Imperial to Metric Conversion
    Converts weight and height from imperial measurements to metric, and
    reverse.
    version 0.1

---
+++ conversion.js
@@ -0,0 +1,40 @@
+//  JQuery to carry out the calcuations 
+$(document).ready(function() {
+
+	// convert feet ins to cms
+	$("#itom1").click(function() {
+		var feet = $("#feet").val() ;
+		var ins = $("#ins").val() ;
+		var total = Math.round((parseInt(feet * 12) + parseInt(ins)) * 2.54) ;
+		$("#cms").val(total) ;
+		} ) ;
+		
+	// convert cms to feet ins
+	$("#mtoi1").click(function() {
+		var cms = $("#cms").val() ;
+		var total = Math.round((parseInt(cms) / 2.54)) ;
+		var tot1 = Math.floor(total / 12) 
+		var tot2 = total % 12 ;
+		$("#feet").val(tot1) ;
+		$("#ins").val(tot2) ;
+		} ) ;
+		
+	// convert stones pounds to kgs
+	$("#itom2").click(function() {
+		var stones = $("#stones").val() ;
+		var pounds = $("#pounds").val() ;
+		var total = Math.round((parseInt(stones * 14) + parseInt(pounds)) / 2.20462) ;
+		$("#kgs").val(total) ;
+		} ) ;
+		
+	// convert kgs to stones pounds
+	$("#mtoi2").click(function() {
+		var kgs = $("#kgs").val() ;
+		var total = Math.round((parseInt(kgs) * 2.20462)) ;
+		var tot1 = Math.floor(total / 14) 
+		var tot2 = total % 14 ;
+		$("#stones").val(tot1) ;
+		$("#pounds").val(tot2) ;
+		} ) ;
+		
+	} ) ;
